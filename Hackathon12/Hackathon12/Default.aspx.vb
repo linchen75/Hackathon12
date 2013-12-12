7dd71f96a33206171a66c057ca51929a3d25d5e9
linchen75 <linchen75@hotmail.com>
2013-12-12 18:12:46 +0800

    Merge

--- Hackathon12/Hackathon12/Default.aspx.vb
+++ Hackathon12/Hackathon12/Default.aspx.vb
@@ -1,8 +1,14 @@
-﻿Public Class _Default
+﻿Imports Hackathon12.DB
+Imports Hackathon12.DomainObject
+
+Public Class _Default
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        Dim oCompanies As List(Of Company) = New CompanyRepository().GetAll()
+        For Each oCompany As Company In oCompanies
+            Console.WriteLine(oCompany.NameProperty)
+        Next
     End Sub
 
 End Class
